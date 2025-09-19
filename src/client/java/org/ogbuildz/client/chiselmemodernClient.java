40866a55f14df3232d29067eaf9b4f2df59a1f59
landocoderissian <126412873+landocoderissian@users.noreply.github.com>
2025-09-19 19:43:16 -0400

    Initial commit

---
+++ src/client/java/org/ogbuildz/client/chiselmemodernClient.java
@@ -0,0 +1,10 @@
+package org.ogbuildz.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class chiselmemodernClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/org/ogbuildz/chiselmemodern.java
@@ -0,0 +1,10 @@
+package org.ogbuildz;
+
+import net.fabricmc.api.ModInitializer;
+
+public class chiselmemodern implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
